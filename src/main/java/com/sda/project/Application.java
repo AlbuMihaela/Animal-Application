d7fc37f4eacbd544094d92b01c0679a12a2603ad
mihaela albu <albumihaela87@yahoo.com>
2021-10-20 02:52:47 +0300

    prepare spring boot application

--- src/main/java/com/sda/project/Application.java
+++ src/main/java/com/sda/project/Application.java
@@ -1,7 +1,13 @@
 package com.sda.project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
         System.out.println("Hello");
     }
 }
