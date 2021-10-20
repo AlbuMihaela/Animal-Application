96d7acdf3f625ff4f49753a05b0a4b8b26984d48
madalina viziteu <elenamadalina.viziteu@gmail.com>
2021-10-20 20:08:16 +0300

    add registration page update user controller

--- src/main/java/com/sda/project/controller/UserController.java
+++ src/main/java/com/sda/project/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.sda.project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class UserController {
+
+    @GetMapping(value="/registration")
+    public String getRegistrationPage(){
+        return "registration";
+    }
 }
