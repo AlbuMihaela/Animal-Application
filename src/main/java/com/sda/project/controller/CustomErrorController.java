d7a44c51e8622ca582b5f96a5f6e0b27c743011e
cosmin bucur <cosmin.bucur@kambi.com>
2021-11-16 22:19:29 +0200

    code review - add error pages

---
+++ src/main/java/com/sda/project/controller/CustomErrorController.java
@@ -0,0 +1,33 @@
+package com.sda.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomErrorController.class);
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/error-500";
+            }
+        }
+        return "error/error-500";
+    }
+}
